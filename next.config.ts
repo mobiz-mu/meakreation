92cd5590961572c7d5f76637456bd72fa1f9510d
GeoHomes1806 <cluthanm@gmail.com>
2026-05-05 14:48:33 +0400

    Disable Vercel image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: false,
 
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
       { protocol: "https", hostname: "images.unsplash.com" },
